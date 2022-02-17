a60013aa9713b9b5d8f09528d9d6a8addc00af9f
Fenil Shah <fn777214@dal.ca>
2022-02-17 11:32:29 -0400

    created project structure and added required resources

---
+++ src/utils/constants.js
@@ -0,0 +1,4 @@
+export const LINKEDIN_URL='https://www.linkedin.com/in/fenil-shah-74030a148/';
+export const GITHUB_URL='https://github.com/FShah26?tab=repositories';
+export const FB_URL='https://www.facebook.com/fenil.shah.127/';
+export const INSA_URL='https://www.instagram.com/fenilshah78';
