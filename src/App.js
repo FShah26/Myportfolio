3378b10735a956d767f6a38b7fc2be950d7bfb4b
Fenil Shah <fn777214@dal.ca>
2022-02-17 14:43:48 -0400

    Implemented responsive NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useEffect,useState }  from 'react';
+import NavBar from './components/NavBar';
+import $ from 'jquery';
 
 function App() {
+  const [loader,setLoader] = useState(true);
+
+  
+
+  useEffect(() =>
+  {
+    // showLoader();
+    
+    setTimeout(()=>hideLoader(),2000);
+    $(window).on('load', function(){
+      $('#preloader').delay(2050).fadeOut('slow');
+      $('body').delay(3850).css({'overflow':'visible'});
+  });
+
+},[]);
+
+const hideLoader = () => setLoader(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      {loader && <div id="preloader">
+          <div id="status"></div> 
+      </div> 
+      }
+      <NavBar/>
+      </div>
   );
 }
 
---
+++ src/components/NavBar.js
@@ -0,0 +1,96 @@
+import React, {useEffect} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/App.css';
+import {FaUserAlt, FaAddressBook,FaPhoneAlt,FaGraduationCap,FaBriefcase,FaLaptopCode,FaMedal} from "react-icons/fa"; 
+import $ from 'jquery';
+export default function NavBar()
+{
+
+  const MenuTogglerClose = () => {
+    $(window).on('load', function(){
+      $('#preloader').delay(2050).fadeOut('slow');
+      $('body').delay(3850).css({'overflow':'visible'});
+  });
+    $(".toggler-menu").on('click', function(){
+      $('.header-left').stop().toggleClass('menu-open');
+      });
+  
+    $('.header-left a').on('click', function(){
+      var toggle = $('.toggler-menu');
+      });
+  }
+
+  useEffect(()=>{
+    MenuTogglerClose();
+},[]);
+    return(
+    <div>
+      <div className='mob-header'>
+        <div className='d-flex'>
+          <div className='navbar-brand'>
+          </div>
+          <button className='toggler-menu'>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </div>
+      <header className="header-left wow slideInLeft" id="navbar-collapse-toggle">
+         <div className="top">
+             <a href="#">FS</a>
+         </div>
+         <ul className="nav nav-menu ">               
+             <li>
+                 <a href="#home" className="nav-link active" data-toggle="tooltip"  data-animation="58" data-goto="1" >
+                 <span className="tooltiptext">Home</span>
+                     <FaUserAlt size={30}/>
+                 </a>
+             </li>           
+                 
+             <li>
+                 <a href="#about" className="nav-link" data-toggle="tooltip">
+                 <span className="tooltiptext">About Me</span>
+                     <FaAddressBook size={30}/>
+                 </a>
+             </li>
+             <li>
+                 <a href="#skills" className="nav-link" data-toggle="tooltip">
+                   <span className="tooltiptext">Professional Skills</span>
+                     <FaMedal size={30}/>
+                 </a>
+             </li>
+  
+             <li>
+                 <a href="#experience" className="nav-link" data-toggle="tooltip">
+                 <span className="tooltiptext">Professional Experience</span>
+                     <FaBriefcase size={30}/>
+                 </a>
+             </li>
+
+             <li>
+                 <a href="#education" className="nav-link" data-toggle="tooltip">
+                 <span className="tooltiptext">Education</span>
+                     <FaGraduationCap size={30}/>
+                 </a>
+             </li>                 
+                   
+             <li>
+                 <a href="#projects" className="nav-link" data-toggle="tooltip">
+                 <span className="tooltiptext">Projects</span>
+                     <FaLaptopCode size={30}/>
+                 </a>
+             </li>          
+                      
+             <li>
+                 <a href="#contact" className="nav-link" data-toggle="tooltip">
+                 <span className="tooltiptext">Contact Me</span>
+                     <FaPhoneAlt size={30}/>
+                 </a>
+             </li>             
+         </ul>         
+     </header>
+
+    </div>
+    )
+}
