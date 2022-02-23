5d6cbfa60270d1deac6925dd4df77a7f80b41882
Fenil Shah <fn777214@dal.ca>
2022-02-22 21:53:38 -0400

    Added Data file that will feed the experience, education & certifications data

---
+++ src/data/Data.js
@@ -0,0 +1,97 @@
+export const certificates = [
+    {
+      title:`Oracle Cloud Infrastructure Foundations 2021 Associate`,
+      issue:'Jan 2022',
+      organization:'Oracle',
+      organization_link:`https://education.oracle.com/`,
+      certificate_link:`https://catalog-education.oracle.com/pls/certview/sharebadge?id=5D588B85DB5D60CA945B520F2D239D509C7FFA4879D02594399BE62FCA8124DA`,
+      image:'OCI_Foundations.png',
+      description:`Completed this certification and learned cloud infrastructure and services provided by Oracle Cloud. 
+      Performed hands-on labs on OCI platform using various services such as OCI Compute, OCI DB Systems, OCI Object Storage, OCI Functions & API Gateway`
+  
+    },
+    {
+      title:`Software Architecture`,
+      issue:'Jan 2022',
+      organization:'Coursera',
+      organization_link:`https://www.coursera.org/`,
+      certificate_link:`https://coursera.org/share/c71c3c5e82aeac0eb3618cf782120f2e`,
+      image:'ualberta.png',
+      description:`Learn various concepts about system architecture such as UML diagrams, Kruchten's 4+1 model, different types of architectures, architectural tradeoffs,
+      analyzing and evaluating an architecture & product architecture`
+    },
+    {
+      title:`Scrum: The Basics`,
+      issue:'Jan 2022',
+      organization:'LinkedIn Learning',
+      organization_link:`https://www.linkedin.com/learning/`,
+      certificate_link:`https://www.linkedin.com/learning/certificates/9b8b62e335c18baefb154719dc97ce1edf106580f4cf1016da9d186854fb3a58?trk=share_certificate`,
+      image:'scrum.png',
+      description:`In this course, I learned about SCRUM framework, it's approach to projects, essential roles & teams involved, agile methodologies, tracking scrum progress and other SCRUM fundamentals`
+    },
+    {
+      title:`Agile Software Development`,
+      issue:'Jan 2022',
+      organization:'LinkedIn Learning',
+      organization_link:`https://www.linkedin.com/learning/`,
+      certificate_link:`https://www.linkedin.com/learning/certificates/4389804fadecd2025bdf23f1c7eea30fe0a6aeeae462903d97a1b9568bf7344e?trk=share_certificate`,
+      image:'agile.jpg',
+      description:`This course helped me to create better understanding about agile software development, agile manifesto, the scrum pillars, scrum roles, teams, events & artifacts, agile approaches
+      such as Extreme Programming, Pair Programming, Test-Driven Development, User Stories, Epics & Themes, Estimation techniques & reporting, Kanban v/s Scrum & frameworks for Scaling agile.`
+    },
+    {
+      title:`Server-Side Development with NodeJS, Express and MongoDB`,
+      issue:'Dec 2021',
+      organization:'Coursera',
+      organization_link:`https://www.coursera.org/`,
+      certificate_link:`https://coursera.org/share/3725f61eb7ce8d3f366416284af50a08`,
+      image:'HKST.svg',
+      description:`This course provided a learning about server-side development using MERN stack.I used NodeJS, ExpressJS and MongoDB to create a server-side backend APIs through hands-on and 
+      learned various server-side authentication techniques and sessions and creating a fully-functional Backend-as-a-Service (BaaS)`
+    },
+    {
+      title:`Design Patterns`,
+      issue:'Oct 2021',
+      organization:'Coursera',
+      organization_link:`https://www.coursera.org/`,
+      certificate_link:`https://coursera.org/share/a58dee6d8687303c885a7ed982f98c50`,
+      image:'ualberta.png',
+      description:`In this course, I learned in-depth about Software Design Patterns and SOLID principles and got to know how following them can make the software components Reusable, Flexible and Maintainable`
+    },
+    {
+      title:`Scrum Foundation Professional Certificate`,
+      issue:'Oct 2021',
+      organization:'CertiProf',
+      organization_link:`https://certiprof.com/`,
+      certificate_link:`https://www.credly.com/badges/816a3425-228b-4ed5-aed6-b4ffd1e46a7d/public_url`,
+      image:'SFPC.png',
+      description:`This examination helped me to test my SCRUM knowledge and how the methodology can be applied in conjunction with Agile to solve problems in product development and how it can be scaled throughout the organization.`
+    },
+    {
+      title:`Agile Basics Certified`,
+      issue:'Sept 2021',
+      organization:'Agile Institute',
+      organization_link:`https://agileinstitute.online/`,
+      certificate_link:`https://agileinstitute.online/certificates/02e038ce82ae2150ec91bec3b9a09096/`,
+      image:'agileinst.jpg',
+      description:`This certification helped me to test my knowledge on Agile Methodologies especially, it's application on software developemnt and various techniques that helps to make the development process better and achievable.`
+    },
+    {
+      title:`Building Modern Python Applications on AWS`,
+      issue:'Jul 2021',
+      organization:'Coursera',
+      organization_link:`https://www.coursera.org/`,
+      certificate_link:`https://coursera.org/share/d55850821f934d1c741ab261295fb094`,
+      image:'aws.png',
+      description:`In this course, I learned to work with AWS services to create a complete end-to-end application using Python. I learned to use services such as Amazon Cognito, API Gateway, AWS Lambda to create serverless backend and Amazon EC2, Amazon ECS and EKS for application hosting.`
+    },
+    {
+      title:`Cloud Computing 101`,
+      issue:'May 2021',
+      organization:'AWS Educate',
+      organization_link:`https://aws.amazon.com/training/`,
+      certificate_link:`https://www.credly.com/badges/39b64287-2f40-4963-ab34-9a08974c8afd/public_url`,
+      image:'cc101.png',
+      description:`Through this course, I was able to build my foundation knowledge on cloud computing concepts such as Deployment and Delivery models, Fundamental Cloud architectures,Security and Business concerns realted to it and about various cloud services provided by AWS.`
+    }
+  ];
