357aa90129ee3150c2763197483d4a03e0c503c9
Fenil Shah <fn777214@dal.ca>
2022-02-22 21:40:12 -0400

    Added uuid as key to the duplicated elements

--- src/components/Ratings.js
+++ src/components/Ratings.js
@@ -2,6 +2,7 @@ import '../css/App.css';
 import React  from 'react';
 import {FaRegStar,FaStarHalfAlt,FaStar} from 'react-icons/fa';
 import { Row } from 'react-bootstrap';
+import {v4 as uuidv4} from 'uuid';
 
 export default function Ratings({value}){
     let val = value;
@@ -26,14 +27,14 @@ export default function Ratings({value}){
     const ratings = [];
     for(var i=1;i<=val;i++)
     {
-        ratings.push(<FaStar className="rating-star" size={18} title={ratingTitle}/>);
+        ratings.push(<FaStar key={uuidv4()} className="rating-star" size={18} title={ratingTitle}/>);
     }
     if(isFloat)
-        ratings.push(<FaStarHalfAlt className="rating-star" size={18} title={ratingTitle}/>)
+        ratings.push(<FaStarHalfAlt key={uuidv4()} className="rating-star" size={18} title={ratingTitle}/>)
     let addedElements = ratings.length;
     for(var j=1;j<=5-addedElements;j++)
     {
-        ratings.push(<FaRegStar className="rating-star" size={18} title={ratingTitle}/>);
+        ratings.push(<FaRegStar key={uuidv4()} className="rating-star" size={18} title={ratingTitle}/>);
     }
     return(
         <Row>
