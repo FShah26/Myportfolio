aba0f42e25af2da4351fd2d0948fc00a024e0141
Fenil Shah <fn777214@dal.ca>
2022-02-25 13:20:13 -0400

    Correct the bug-> not redirecting to certificate

--- src/components/SkillCertificationCard.js
+++ src/components/SkillCertificationCard.js
@@ -1,16 +1,16 @@
 import '../css/App.css';
 import React from 'react';
-import {Col,Image} from 'react-bootstrap';
+import {Col} from 'react-bootstrap';
 import {FaExternalLinkAlt, FaHackerrank} from 'react-icons/fa';
 import {FcApproval} from 'react-icons/fc';
-import {SiHackerrank} from 'react-icons/si';
 
 export default function SkillCertificationCard(props) {
     var imagePath = process.env.PUBLIC_URL + `/skillscert/${props.tech}`;
     return(
         <>
+        <a href={props.link} target="_blank" rel="noreferrer">
             <Col xs={2} key={props.title} className="wow bounceInLeft">
-            <a href={props.link} target="_blank" rel="noreferrer">
+            
                 <div className="certified-skills-box" >
                     <div className="text-white">
                         <div className="accordion-item">
@@ -23,8 +23,8 @@ export default function SkillCertificationCard(props) {
                     </div>
                 </div>
                 <br/>
-                </a>
             </Col>
+            </a>
         </>
     );
 }
